cb761b57e3fe84584c3af1e0e20483d10fc3a129
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-02-13 17:16:09 -0500

    Edited player object

--- src/Player.java
+++ src/Player.java
@@ -5,11 +5,52 @@ public class Player {
     private int space = 0;
     private Image ic;
     private ImageView icon;
+    private int money = 0;
 
+    public Player(Image image){
+        icon = new ImageView(image);
+    }
 
     protected void move(int val){
 
     };
+    public int addMoney(int amount){
+        money += amount;
+        return money;
+    }
 
 
+
+
+    public int getSpace() {
+        return space;
+    }
+
+    public void setSpace(int space) {
+        this.space = space;
+    }
+
+    public Image getIc() {
+        return ic;
+    }
+
+    public void setIc(Image ic) {
+        this.ic = ic;
+    }
+
+    public ImageView getIcon() {
+        return icon;
+    }
+
+    public void setIcon(ImageView icon) {
+        this.icon = icon;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
