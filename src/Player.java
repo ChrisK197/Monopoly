78c8af2fb5244508b447d59d1c2cb4ccd1a762f0
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-02-13 17:05:22 -0500

    Made player object

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Player {
+    private int space = 0;
+    private Image ic;
+    private ImageView icon;
+
+
+    protected void move(int val){
+
+    };
+
+
+}
