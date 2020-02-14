fcb221d6d4ea8a9f3fd7a265845f8e55ed2eeeb2
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-02-13 20:32:58 -0500

    Editted Property object

--- src/Property.java
+++ src/Property.java
@@ -7,7 +7,21 @@ public class Property {
     private boolean isPurchased = false;
     private String name;
     private int spacenum;
-    //protected static HashMap<String, Integer> propToNum= {"Brown": 2};
+    protected static HashMap<String, Integer> propToNum = new HashMap<>();
+    static {
+        propToNum.put("purple", 2);
+        propToNum.put("lblue", 3);
+        propToNum.put("pink", 3);
+        propToNum.put("orange", 3);
+        propToNum.put("red", 3);
+        propToNum.put("yellow", 3);
+        propToNum.put("green", 3);
+        propToNum.put("dblue", 2);
+        propToNum.put("railroad", 4);
+        propToNum.put("utility", 2);
+
+    }
+
 
     public Property(int price, int tax, String color, String name, int space){
         this.price = price;
