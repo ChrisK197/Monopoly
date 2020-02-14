65ccf372026406c7804d89c69fae1e1831a285d7
ChrisK197 <chrkal22@bergen.org>
2020-02-14 07:50:08 -0500

    updated card class

--- src/ChanceCard.java
+++ src/ChanceCard.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class ChanceCard {
-    String[] cards = {"Advance to Go", "Advance to Illinois Avenue. If you pass Go, collect $200", "Advance St. Charles Place. ",
-            "If you pass Go, collect $200", "Get out of jail free", "Go back three spaces", "Go to jail. Go directly to jail.",
-            "Pay $25 for each property", "Pay poor tax of $15", "Go to the Reading Railroad. If you pass Go, collect $200",
+    private static String[] cards = {"Advance to Go", "Advance to Illinois Avenue. If you pass Go, collect $200", "Advance St. Charles Place", "If you pass Go, collect $200", "Get out of jail free", "Go back three spaces", "Go to jail. Go directly to jail.", "Pay $25 for each property", "Pay poor tax of $15", "Go to the Reading Railroad. If you pass Go, collect $200",
             "You have been elected Chairman of the Board. Pay each player $50",
             "Your investments are soaring. Collect $150", "You have won a crossword competition. Collect $100"};
 
-    public ChanceCard () {}
+    private String cardPhrase;
+
+    public static int total = cards.length;
+
+    public ChanceCard () {
+        cardPhrase = cards[(int)(Math.random()*total)];
+    }
+
+    public String getCardPhrase () {
+        return cardPhrase;
+    }
 
 }
