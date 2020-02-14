3821fd6fdf755c4bbc793cf464875f2bc34c118c
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-02-14 04:17:33 -0500

    Let the game end

--- src/MainFile.java
+++ src/MainFile.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.beans.property.StringProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
