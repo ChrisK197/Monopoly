de337b2f894a2358936bc005c58ea1d541e4cce9
Leo Ciccarelli <lciccarelli74@gmail.com>
2020-02-13 19:55:05 -0500

    Editted Property object

--- src/Property.java
+++ src/Property.java
@@ -6,13 +6,15 @@ public class Property {
     private String color;
     private boolean isPurchased = false;
     private String name;
+    private int spacenum;
     //protected static HashMap<String, Integer> propToNum= {"Brown": 2};
 
-    public Property(int price, int tax, String color, String name){
+    public Property(int price, int tax, String color, String name, int space){
         this.price = price;
         this.tax = tax;
         this.color = color;
         this.name = name;
+        spacenum = space;
     }
 
 
